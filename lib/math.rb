5c96a25e2861ae299ff9dcac8efbb1ce15a315b2
Sharrieff ross <crross@dons.usfca.edu>
2019-08-14 03:08:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
-def addition(num1, num2)
-
+def addition(num1="5", num2="4")
+ puts "hi"
+ num1 + num2
 end
 
-def subtraction(num1, num2)
-
+def subtraction(num1="10", num2="5")
+  puts "bye"
+  num1 - num2
 end
 
-def division(num1, num2)
-
+def division(num1="50", num2="2")
+  puts "run"
+  num1 / num2
 end
 
-def multiplication(num1, num2)
-
+def multiplication(num1="4", num2="30")
+  puts "hot"
+  num1 * num2
 end
 
-def modulo(num1, num2)
-
+def modulo(num1="16", num2="4")
+  puts "thunder"
+  num1 % num2 = 4
 end
 
-def square_root(num)
-
+def square_root(num="81")
+  puts "dog"
+  Math.sqrt(num)
 end
